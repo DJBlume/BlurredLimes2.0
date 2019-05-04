c78e5c48cb8389a1fb315178dcfae407b5620ba3
DJ Blume <dj.blume9@gmail.com>
2019-05-04 17:21:17 +0000

    Attempting to connect

---
+++ BlurredLimes/BlurredLimes2.js
@@ -0,0 +1,25 @@
+var getStarted = document.getElementById("formButton");//grab "get started" button
+var addIngredient = document.getElementById("addIngredient");
+
+getStarted.addEventListener("click", function(){ //add click listener to "get started" button
+    var display = document.getElementById("form");
+    display.classList.toggle("display"); //toggle class that displays the form  
+});
+
+addIngredient.addEventListener("click", function(){
+    var newLi = document.createElement('li');
+    var newInput = document.createElement('input');
+    var ingredient = document.createTextNode('Ingredient: ');
+    
+    newLi.appendChild(ingredient);
+	newLi.appendChild(newInput);
+	
+	var position = document.getElementById('list');
+	
+	position.appendChild(newLi);
+	
+});
+
+app.listen(3000, function(){
+	console.log("Server has started");
+});
